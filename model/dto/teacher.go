a9c7ee08c655ab19bb4ee3d17d7b8e4b8d21827f
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-13 14:49:18 +0800

    fix: move TeacherDTO into teacher.go and add email field

--- model/dto/teacher.go
+++ model/dto/teacher.go
@@ -1,1 +1,10 @@
 package dto
+
+type TeacherDTO struct {
+	ID         int64  `json:"id"`
+	Email      string `json:"email"`
+	Code       string `json:"code"`
+	Name       string `json:"name"`
+	Department string `json:"department"`
+	Title      string `json:"title"`
+}
