e973b864f1cbf980665ccd73ac57c76c27efd70e
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-13 16:20:52 +0800

    feat: basic convertor, dto, domain and po for teacher

--- model/converter/teacher.go
+++ model/converter/teacher.go
@@ -2,6 +2,7 @@ package converter
 
 import (
 	"jcourse_go/model/domain"
+	"jcourse_go/model/dto"
 	"jcourse_go/model/po"
 )
 
@@ -9,10 +10,23 @@ func ConvertTeacherPOToDomain(teacher *po.TeacherPO) *domain.Teacher {
 	if teacher == nil {
 		return nil
 	}
+
 	return &domain.Teacher{
 		ID:         int64(teacher.ID),
-		Code:       teacher.Code,
 		Name:       teacher.Name,
+		Email:      teacher.Email,
+		Code:       teacher.Code,
+		Department: teacher.Department,
+		Title:      teacher.Title,
+	}
+}
+
+func ConvertTeacherDomainToDTO(teacher domain.Teacher) dto.TeacherDTO {
+	return dto.TeacherDTO{
+		ID:         teacher.ID,
+		Email:      teacher.Email,
+		Name:       teacher.Name,
+		Code:       teacher.Code,
 		Department: teacher.Department,
 		Title:      teacher.Title,
 	}
--- model/domain/teacher.go
+++ model/domain/teacher.go
@@ -3,6 +3,7 @@ package domain
 type Teacher struct {
 	ID         int64
 	Name       string
+	Email      string
 	Code       string
 	Department string
 	Title      string
