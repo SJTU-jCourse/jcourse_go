be668cbb5cb9715e8bcf875e4dfd5d4d6fcf5e22
Du Jiajun <jiajun_du@outlook.com>
2025-09-24 16:02:57 +0800

    feat: remove unused struct definition

--- internal/interface/olddto/rating.go
+++
@@ -1,7 +0,0 @@
-package olddto
-
-type RatingDTO struct {
-	RelatedType string `json:"related_type" binding:"required"`
-	RelatedID   int64  `json:"related_id" binding:"required"`
-	Rating      int64  `json:"rating" binding:"required"`
-}
